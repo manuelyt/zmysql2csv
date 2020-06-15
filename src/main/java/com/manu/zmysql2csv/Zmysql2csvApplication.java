ee5c10dbd73fa55e839d74767e0f71d322eb4e01
manu-yanez <manuel.yanez@twtspain.com>
2020-06-15 16:48:25 +0200

    aaab

---
+++ src/main/java/com/manu/zmysql2csv/Zmysql2csvApplication.java
@@ -0,0 +1,13 @@
+package com.manu.zmysql2csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zmysql2csvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zmysql2csvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manu/zmysql2csv/Zmysql2csvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manu.zmysql2csv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zmysql2csvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
